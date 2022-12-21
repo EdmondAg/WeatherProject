dcb55882804bb2d7cb6f739ee21ad215586de7ab
Edmond <97a.edmond@gmail.com>
2022-12-21 12:29:30 +0100

    init commit

--- src/main/java/org/example/coinapi/Main.java
+++ src/main/java/org/example/coinapi/Main.java
@@ -1,9 +1,27 @@
 package org.example.coinapi;
 
-import org.example.URL;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import static org.example.coinapi.URManager.FULL_URL;
 
 public class Main {
     public static void main(String[] args) {
-        URL url = URL.WEATHER;
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(FULL_URL)
+                .addHeader("X-CoinAPI-Key", "73034021-THIS-IS-SAMPLE-KEYs")
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            assert response.body() != null;
+            System.out.println(response.body().string());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
---
+++ src/main/java/org/example/coinapi/URManager.java
@@ -0,0 +1,9 @@
+package org.example.coinapi;
+
+public class URManager {
+    final static String FULL_URL = "https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=DF197913-034E-461D-98EE-E39AF52E4D28";
+    final static String BASE_URL = "https://rest.coinapi.io/v1/";
+    final static String EXCHANGE_RATE = "exchangerate/";
+    final static String KEY = "DF197913-034E-461D-98EE-E39AF52E4D28/";
+
+}
